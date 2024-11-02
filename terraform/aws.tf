84561cb3c24e166f318643f941b2102809cf73c7
celeguim <luiz.celeguim@gmail.com>
2024-11-02 21:59:27 +0100

    security group updates

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,19 +1,34 @@
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.useast1.id
+resource "aws_security_group" "sg1" {
+  name   = "sg1"
+  vpc_id = aws_vpc.useast1.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.allow_ssh.id
+  security_group_id = aws_security_group.sg1.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "tcp"
   from_port         = 22
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.sg1.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tomcat_ipv4" {
+  security_group_id = aws_security_group.sg1.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 8080
+  to_port           = 8080
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_ssh.id
+  security_group_id = aws_security_group.sg1.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # Egress for all ports
 }
@@ -28,7 +43,7 @@ resource "aws_instance" "nginx" {
   ami                    = var.ami_id_aws
   instance_type          = var.instance_type
   key_name               = aws_key_pair.default.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.sg1.id]
   subnet_id              = aws_subnet.useast1_public[0].id
 
   tags = {
@@ -41,7 +56,7 @@ resource "aws_instance" "tomcat" {
   ami                    = var.ami_id_ubuntu
   instance_type          = var.instance_type
   key_name               = aws_key_pair.default.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.sg1.id]
   subnet_id              = aws_subnet.useast1_private[0].id
 
   tags = {
@@ -90,14 +105,14 @@ resource "ansible_host" "tomcat" {
     # ec2-user for Amazon Linux
     ansible_user            = "ubuntu"
     ansible_host            = aws_instance.tomcat.private_ip
-    ansible_ssh_common_args = "-o ProxyCommand='ssh -p 22 -W %h:%p -i ~/.ssh/id_rsa ubuntu@${aws_instance.nginx.public_ip}'"
+    ansible_ssh_common_args = "-o ProxyCommand='ssh -p 22 -W %h:%p -i ~/.ssh/id_rsa ec2-user@${aws_instance.nginx.public_ip}'"
 
     # Custom vars.
     hostname = "tomcat"
     fqdn     = "tomcat.example.com"
 
     # To define lists or maps use jsonencode().
-    packages = jsonencode(["openjdk-11-jre", "tomcat10", "tomcat10-admin"])
+    packages = jsonencode(["openjdk-17-jdk", "tomcat10", "tomcat10-examples"])
 
     map_var = jsonencode({
       provider    = "AWS",
